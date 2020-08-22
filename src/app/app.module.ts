e0154bb8c2ff4b5981a1ac0c31adec55f50c3cb7
Shivam Biswas <shivamksg12@gmail.com>
2020-08-22 19:12:50 +0530

    adding menu component v1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,30 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+//imported to use angular material, flex-layout and angular animations
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatToolbarModule } from '@angular/material/toolbar'; 
+import { FlexLayoutModule } from '@angular/flex-layout';
+//still to find what is the essense of this
+import 'hammerjs';
+import { MenuComponent } from './menu/menu.component';
+
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    FlexLayoutModule,
     AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule { 
+  title='conFusion';
+}
