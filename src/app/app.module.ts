fd36abd7ff51eb587580a6cfba0fd2b24198aa8a
Shivam Biswas <shivamksg12@gmail.com>
2020-09-26 15:43:45 +0530

    angular-routing basics

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,33 +1,47 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 //imported to use angular material, flex-layout and angular animations
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatToolbarModule } from '@angular/material/toolbar'; 
+import { MatToolbarModule } from '@angular/material/toolbar';
 import { FlexLayoutModule } from '@angular/flex-layout';
-//still to find what is the essense of this
-import 'hammerjs';
-import { MenuComponent } from './menu/menu.component';
-
 //to import mat-grid-list
 import { MatGridListModule } from '@angular/material/grid-list';
-
 //to import button
 import { MatButtonModule } from '@angular/material/button';
 import { MatListModule } from '@angular/material/list';
 //to import mat-card
-
 import { MatCardModule } from '@angular/material/card';
+
+//still to find what is the essense of this
+import 'hammerjs';
+
+//components
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { AboutComponent } from './about/about.component';
+import { HomeComponent } from './home/home.component';
+import { ContactComponent } from './contact/contact.component';
+import { MenuComponent } from './menu/menu.component';
 import { DishdetailComponent } from './dishdetail/dishdetail.component';
+
+//services
+import { DishService } from './services/dish.service';
+import { AppRoutingModule } from './app-routing/app-routing.module';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     MenuComponent,
-    DishdetailComponent
+    DishdetailComponent,
+    HeaderComponent,
+    FooterComponent,
+    AboutComponent,
+    HomeComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
@@ -40,9 +54,9 @@ import { DishdetailComponent } from './dishdetail/dishdetail.component';
     MatCardModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [DishService],
   bootstrap: [AppComponent]
 })
 export class AppModule { 
-  title='conFusion';
+  title = 'conFusion';
 }
